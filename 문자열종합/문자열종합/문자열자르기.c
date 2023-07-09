f5440764a7279f9e77df8f6534d96aee21250dae
Paranonk <codeparadoxist@gmail.com>
2023-07-10 05:32:24 +0900

    문자배열 자르기

--- 문자열종합/문자열종합/문자열자르기.c
+++ 문자열종합/문자열종합/문자열자르기.c
@@ -2,18 +2,13 @@
 #include <stdio.h>
 
 int main() {
-	//포인터자르기
-	char* s1 = malloc(sizeof(char) * 30);
-	strcpy(s1, "The Little Prince");
+	char s1[30] = "The Little Prince";
 
 	char* ptr = strtok(s1, " ");
-
-	
 
 	while (ptr != NULL) {
 		printf("%s\n", ptr);
 		ptr = strtok(NULL, " ");
 	}
 
-	free(s1);
 }
