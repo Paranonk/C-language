e93ea5cd07cd46487d10c8ab45c58b028e3662b0
Paranonk <codeparadoxist@gmail.com>
2023-07-10 05:38:46 +0900

    문자열 시간 자르기

--- 문자열종합/문자열종합/문자열자르기.c
+++ 문자열종합/문자열종합/문자열자르기.c
@@ -1,14 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+#include <string.h>
 int main() {
-	char s1[30] = "The Little Prince";
+	char s1[30] = "2015-06-10T15:32:19";
 
-	char* ptr = strtok(s1, " ");
+
+
+	char* ptr = strtok(s1, "-T:");
 
 	while (ptr != NULL) {
+	
 		printf("%s\n", ptr);
-		ptr = strtok(NULL, " ");
+		ptr = strtok(NULL, "-T:");
 	}
 
 }
